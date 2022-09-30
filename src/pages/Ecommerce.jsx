9821af86ed251a6311d01cda648207c39a50521d
elbasel <abdelrahman.elbasel@gmail.com>
2022-09-30 20:45:09 +0200

    working on ecommerce homepage

---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Button } from "../components";
+import { earningData } from "../data/dummy";
+
+const Ecommerce = () => {
+  return (
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-xl lg:w-80 p-8 pt-9 m-3 bg-hero-pattern w-full h-44 bg-cover bg-no-repeat bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="font-bold text-gray-400">Earnings</p>
+              <p className="text-2xl">$43,323,233</p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <Button
+              color="white"
+              backgroundColor="blue"
+              text="Download"
+              borderRadius="10px"
+              size="md"
+            />
+          </div>
+        </div>
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-46 p-4 pt-9 rounded-2xl"
+            >
+              <button
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-90 rounded-full p-4 hover:drop-shadow-xl"
+              >
+                {item.icon}
+              </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ecommerce;
